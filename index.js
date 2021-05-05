cdefa8104548161a62ee5edae926983ed31d917a
Zhino Salam <zhinosalam188@gmail.com>
2021-05-05 20:58:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 // Your code here
-function razzle() {
-  console.log("You've been razzled!");
+unction saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
 }
-razzle();
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair="*") {
+  return function(adj="special") {
+    return `You are ${flair }${adj}${flair}!`;
+  }
+}
+
+const Calculator = {
+  add: function(a, b) {
+    return a+b;
+  },
+  subtract: function(a, b) {
+    return a-b;
+  },
+  multiply: function(a, b) {
+    return a*b;
+  },
+  divide: function(a, b) {
+    return a/b;
+  }
+}
+
+function actionApplyer(start, fns) {
+  for (let i = 0; i < fns.length; i++) {
+    start = fns[i](start);
+  }
+  return start;
+}
